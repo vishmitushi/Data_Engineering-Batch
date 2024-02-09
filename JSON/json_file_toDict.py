46c636ee3e5aaeba33ec0b92906542dc29f21885
Mitushi Vishwakarma <68201079+vishmitushi@users.noreply.github.com>
2024-02-09 23:15:07 +0530

    Delete JSON directory

--- JSON/json_file_toDict.py
+++
@@ -1,8 +0,0 @@
-import json
-with open('JSON\data.json') as json_file:
-    data = json.load(json_file)
-    print(type(data))
-    print(data)
-    print(data['employee'][0]['name'])
-    for i in data['employee']:
-        print(i)
