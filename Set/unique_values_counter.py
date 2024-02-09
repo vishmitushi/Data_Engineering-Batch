9f4e5f7928d657484cce6ffe743ff796e9f42a1e
Mitushi Vishwakarma <68201079+vishmitushi@users.noreply.github.com>
2024-02-09 23:16:13 +0530

    Delete Set directory

--- Set/unique_values_counter.py
+++
@@ -1,8 +0,0 @@
-from collections import Counter
-def unique(l):
- counter = Counter(l)
- print(*counter)
-
-l1 = [10, 20, 10, 30, 40, 40]
-print("the unique values from the list is")
-unique(l1)
--- Set/unique_values_fromkeys.py
+++
@@ -1,4 +0,0 @@
-lst = [1, 2, 1, 1, 3, 4, 3, 3, 5]
-
-unique_list_1 = list(dict.fromkeys(lst))
-print(unique_list_1)
